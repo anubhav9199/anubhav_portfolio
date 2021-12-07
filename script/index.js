7f1364327dc91be7f6a59530362b611b46ec3f9c
Anubhav Sharma <anubhav.sharma@medcords.com>
2021-12-07 19:52:09 +0530

    Creating about and service page

--- script/index.js
+++ script/index.js
@@ -63,6 +63,44 @@
 //   new TypeWriter(txtElement, words, wait);
 // }
 
+var demoButtons;
+
+function start () {
+  
+  // Add event "click" to "demo buttons"
+  demoButtons = document.querySelectorAll ('.js-modify');
+  for (var i = 0; i < demoButtons.length; i++) {
+    demoButtons[i].addEventListener ('click', toggleEffect);
+  }
+  
+  // Add event "click" to "save buttons"
+  var saveButtons = document.querySelectorAll ('.js-save');
+  for (var i = 0; i < saveButtons.length; i++) {
+    saveButtons[i].addEventListener ('click', toggleActive);
+  }
+  
+}
+
+// Toggle "effect" classes
+function toggleEffect () {
+  var target = document.querySelector (this.dataset.target);
+      target.dataset.effect = this.dataset.effect;
+  
+  for (var i= 0; i < demoButtons.length; i++) {
+    demoButtons[i].classList.remove ('active');
+  }
+  
+  toggleActive.call (this);
+}
+
+// Toggle "active" class
+function toggleActive () {
+  this.classList.toggle ('active');
+}
+
+// Invoke "start ()" function
+window.addEventListener ('load', start);
+
 
 $(function() {
   var isMobile;
@@ -148,9 +186,9 @@ $(function() {
     if (pos2 > $('#about').offset().top) {
       highlightLink('about');
     }
-    // if (pos2 > $('#services').offset().top) {
-    //   highlightLink('services');
-    // }
+    if (pos2 > $('#services').offset().top) {
+      highlightLink('services');
+    }
     // if (pos2 > $('#current-project').offset().top) {
     //   highlightLink('current-project');
     // }
