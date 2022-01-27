eb32f81432df16badd9a2953b04da105df08f976
Anubhav Sharma <anubhavsharma9199@gmail.com>
2022-01-27 23:45:43 +0530

    fixing navbar script for the page

--- script/index.js
+++ script/index.js
@@ -160,24 +160,24 @@ $(function() {
   window.onload=animateText;
 
   // NAV POSITION
-  // var navPos = $('nav').position().top;
-  // var lastPos = 0;
-  // var lockTimer;
+  var navPos = $('nav').position().top;
+  var lastPos = 0;
+  var lockTimer;
 
-  // $(window).on('scroll', function() {
-  //   var pos = $(window).scrollTop();
-  //   var pos2 = pos + 50;
-  //   var scrollBottom = pos + $(window).height();
+  $(window).on('scroll', function() {
+    var pos = $(window).scrollTop();
+    var pos2 = pos + 50;
+    var scrollBottom = pos + $(window).height();
 
-  //   if (!isMobile) {
-  //     if (pos >= navPos + $('nav').height() && lastPos < pos) {
-  //       $('nav').addClass('fixed');
-  //     }
-  //     if (pos < navPos && lastPos > pos) {
-  //       $('nav').removeClass('fixed');
-  //     }
-  //     lastPos = pos;
-  //   }
+    if (!isMobile) {
+      if (pos >= navPos + $('nav').height() && lastPos < pos) {
+        $('nav').addClass('fixed');
+      }
+      if (pos < navPos && lastPos > pos) {
+        $('nav').removeClass('fixed');
+      }
+      lastPos = pos;
+    }
 
     // Link Highlighting
     if (pos2 > $('#home').offset().top) {
